333aac27092cc462040a9d46e2bb4eb0c90e6a60
Alimnfl <mnaufallim234@gmail.com>
2023-09-23 11:32:48 +0700

    Updated AnimeCollection ALL

--- src/context/animeCollections.tsx
+++ src/context/animeCollections.tsx
@@ -1,5 +1,5 @@
-import { createContext, useContext } from 'react';
-import { AnimeCollectionsContextType } from '../types/types';
+import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
+import { AnimeCollectionsContextType, CollectionAnimesType, CollectionType } from '../types/types';
 
 const AnimeCollectionsContext = createContext<AnimeCollectionsContextType>({
   collections: [],
@@ -11,6 +11,78 @@ const AnimeCollectionsContext = createContext<AnimeCollectionsContextType>({
   removeCollection: () => {},
 });
 
+const AnimeCollectionsProvider = ({ children }: { children: ReactNode }) => {
+  const [collections, setCollections] = useState(() => {
+    // Ngambil data di local storage
+    const storedCollections = JSON.parse(localStorage.getItem('myAnimeCollections')!) || [];
+    return storedCollections;
+  });
+
+  // Nambah koleksi
+  const addNewCollection = (newCollection: CollectionType) => {
+    setCollections((prev: any) => [...prev, newCollection]);
+  };
+
+  // Nambahin anime ke koleksi
+  const addAnimeToCollection = (collectionId: string, anime: string, idAnime: string, coverImage: string) => {
+    const addAnime = {
+      id: `${Date.now()}`,
+      id_anime: idAnime,
+      title: anime,
+      coverImage,
+    };
+    setCollections((prevCollections: CollectionType[]) =>
+      prevCollections.map((collection: CollectionType) => {
+        collection.id === collectionId ? { ...collection, animes: [...collection.animes, addAnime] } : collection;
+      })
+    );
+  };
+
+  // Edit Koleksi
+  const editCollection = (collectionId: string, newName: string, newDescription: string) => {
+    setCollections((prevCollections: CollectionType[]) =>
+      prevCollections.map((collection: CollectionType) =>
+        collection.id === collectionId
+          ? {
+              ...collection,
+              name: newName,
+              description: newDescription,
+            }
+          : collection
+      )
+    );
+  };
+
+  // Menghapus anime dari collection
+  const removeAnimeFromCollection = (collectionId: string, animeId: string) => {
+    setCollections((prevCollections: CollectionType[]) =>
+      prevCollections.map((collection: CollectionType) =>
+        collection.id === collectionId
+          ? {
+              ...collection,
+              animes: collection.animes.filter((anime: CollectionAnimesType) => anime.id !== animeId),
+            }
+          : collection
+      )
+    );
+  };
+
+  // Menghapus Collection
+  const removeCollection = (collectionId: string) => {
+    setCollections((prevCollections: CollectionType[]) => prevCollections.filter((collection: CollectionType) => collection.id !== collectionId));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('myAnimeCollections', JSON.stringify(collections));
+  }, [collections]);
+
+  return (
+    <>
+      <AnimeCollectionsContext.Provider value={{ collections, setCollections, addNewCollection, addAnimeToCollection, editCollection, removeAnimeFromCollection, removeCollection }}>{children}</AnimeCollectionsContext.Provider>
+    </>
+  );
+};
+
 const useAnimecollections = () => {
   const context = useContext(AnimeCollectionsContext);
 
@@ -19,4 +91,4 @@ const useAnimecollections = () => {
   return context;
 };
 
-export { useAnimecollections };
+export { useAnimecollections, AnimeCollectionsProvider };
