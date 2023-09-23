fccb5375863bb0585c4c707a79371f84a475cee9
Alimnfl <mnaufallim234@gmail.com>
2023-09-23 11:32:33 +0700

    Updated Navbar for Collection

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -8,15 +8,17 @@ function Sidebar() {
   const { addNewCollection, collections } = useAnimecollections();
 
   return (
-    <div className="flex flex-row items-center gap-5 text-white p-7">
-      <AiOutlineBars size={30} />
-      <Link to={'/collection'}>
-        <h1 className="text-xl font-bold cursor-pointer">Your Collection</h1>
-      </Link>
+    <div className="flex flex-col">
+      <div className="flex flex-row items-center gap-5 text-white p-7">
+        <AiOutlineBars size={30} />
+        <Link to={'/collection'}>
+          <h1 className="text-xl font-bold cursor-pointer">Your Collection</h1>
+        </Link>
 
-      <AddCollectionModal collections={collections} onAddCollection={addNewCollection}>
-        <AiOutlinePlus size={30} className="ml-[80px] cursor-pointer" />
-      </AddCollectionModal>
+        <AddCollectionModal collections={collections} onAddCollection={addNewCollection}>
+          <AiOutlinePlus size={30} className="ml-[80px] cursor-pointer" />
+        </AddCollectionModal>
+      </div>
 
       {collections.map((collection: CollectionType) => (
         <Link to={`/collection/${collection.id}`}>
@@ -34,16 +36,18 @@ function Navbar() {
   return (
     <div className="fixed flex flex-col gap-3 ">
       <div className="flex flex-col justify-between py-14 px-5 w-[400px] h-[300px]  rounded-r-lg  bg-zinc-950">
-        <div className="flex flex-row gap-8 ml-4">
-          <img className="rounded-lg w-7" src="/logo-lim.svg" />
-          <Link to="/">
+        <Link to="/">
+          <div className="flex flex-row gap-8 ml-4">
+            <img className="rounded-lg cursor-pointer w-7" src="/logo-lim.svg" />
             <p className="text-2xl font-bold text-white">LimNfl</p>
-          </Link>
-        </div>
-        <div className="flex flex-row items-center gap-8 ml-4">
-          <AiFillHome size={28} className="text-white" />
-          <p className="text-2xl font-bold text-white">Anime</p>
-        </div>
+          </div>
+        </Link>
+        <Link to="/">
+          <div className="flex flex-row items-center gap-8 ml-4">
+            <AiFillHome size={28} className="text-white" />
+            <p className="text-2xl font-bold text-white">Anime</p>
+          </div>
+        </Link>
       </div>
       <div className="w-[400px] h-[700px] rounded-tr-lg  bg-zinc-950">
         <Sidebar />
