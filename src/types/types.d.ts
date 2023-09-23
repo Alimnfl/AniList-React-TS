da0fb9d25ec11c59f26e9240042fd13b305a0edd
Alimnfl <mnaufallim234@gmail.com>
2023-09-23 11:33:41 +0700

    Updated Typing for TS

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,3 +1,30 @@
+interface AnimeType {
+  id: number;
+  coverImage: {
+    color: string;
+    large: string;
+    medium: string;
+    extraLarge: string;
+    __typename: string;
+  };
+  description: string;
+  popularity: number;
+  title: {
+    romaji: string;
+    __typename: string;
+  };
+}
+
+export interface AnimeCollectionsContextType {
+  collections: CollectionType[];
+  setCollections: Dispatch<SetStateAction<CollectionType[]>>;
+  addNewCollection: (newCollection: CollectionType) => void;
+  addAnimeToCollection: (collectionId: string, anime: string, idAnime: string, coverImage: string) => void;
+  editCollection: (collectionId: string, anime: string, idAnime: string, coverImage: string) => void;
+  removeAnimeFromCollection: (collectionId: string, animeId: string) => void;
+  removeCollection: (collectionId: string) => void;
+}
+
 export interface CollectionAnimesType {
   coverImage: string;
   id: string;
@@ -12,12 +39,8 @@ export interface CollectionType {
   name: string;
 }
 
-export interface AnimeCollectionsContextType {
-  collections: CollectionType[];
-  setCollections: Dispatch<SetStateAction<CollectionType[]>>;
-  addNewCollection: (newCollection: CollectionType) => void;
-  addAnimeToCollection: (collectionId: string, anime: string, idAnime: string, coverImage: string) => void;
-  editCollection: (collectionId: string, anime: string, idAnime: string, coverImage: string) => void;
-  removeAnimeFromCollection: (collectionId: string, animeId: string) => void;
-  removeCollection: (collectionId: string) => void;
+export interface AddToCollectionType {
+  anime: string;
+  coverImage: string;
+  idAnime: string;
 }
